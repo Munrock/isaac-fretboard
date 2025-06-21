6fc8a8467255c24d71fac2e3fa22a50f0fffd674
Steve Munro <munrock@gmail.com>
2025-06-22 06:34:04 +0800

    Add triadShapes data structure for major chords

--- src/utils/fretboard.ts
+++ src/utils/fretboard.ts
@@ -32,3 +32,19 @@ export const fretboardMap = [
             'E','F','F#','G','G#','A','A#','B']
     }
 ];
+
+export const triadShapes = {
+    major: {
+        String6: {
+            G: [0, -1, -3, -3, -3, 0],
+            E: [0, 2, 2, 1, 0, 0,],
+        },
+        String5: {
+            C: [-1, 3, 2, 0, 1, -1],
+            A: [-1, 0, 2, 2, 2, -1],
+        },
+        String4: {
+            D: [-1, 0, 2, 3, 2, -1],
+        },
+    }
+}
