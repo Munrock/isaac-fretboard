56bdc67ba95983f694a6ac8f2b22c15f52d371b5
munrock <munrock@gmail.com>
2025-07-16 11:06:44 +0800

    add fretbaord viewer to app front

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import styles from "./App.module.css";
 import { AudioProvider } from "./context/AudioContext";
+import Board from "./components/fretboard/Board";
 
 function App() {
   const [activeComponent, setActiveComponent] = useState<string>("ChordView");
 
   return (
     <AudioProvider>
-      <div className={styles.topRow}></div>
+      <div className={styles.topRow}>
+        <button onClick={() => setActiveComponent("ChordView")}>
+          Chord View
+        </button>
+        <button onClick={() => setActiveComponent("FretBoard")}>
+          Fretboard
+        </button>
+      </div>
       <div className={styles.mainPanel}>
         {activeComponent === "ChordView" ? (
           <div className={styles.mainChordDisplay}>
             Amaj<sup>7</sup>
           </div>
+        ) : activeComponent === "FretBoard" ? (
+          <Board />
         ) : (
           ""
         )}
