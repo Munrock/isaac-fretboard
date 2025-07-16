5556b377bac7bfe759e7efc029c72a3e9f84e3a8
munrock <munrock@gmail.com>
2025-07-16 10:19:04 +0800

    Refactor App component to conditionally render mainChordDisplay and remove LastNote and ErrorLog components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import LastNote from './components/LastNote';
-import ErrorLog from './components/ErrorLog';
-import styles from './App.module.css';
-import { AudioProvider } from './context/AudioContext';
-
+import { useState } from "react";
+import styles from "./App.module.css";
+import { AudioProvider } from "./context/AudioContext";
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState<string>("ChordView");
+
   return (
     <AudioProvider>
       <div className={styles.topRow}></div>
       <div className={styles.mainPanel}>
-        <div className={styles.mainChordDisplay}>
-          Amaj<sup>7</sup>
-        </div>
+        {activeComponent === "ChordView" ? (
+          <div className={styles.mainChordDisplay}>
+            Amaj<sup>7</sup>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
       <div className={styles.bottomRow}>
-        <div className={styles.statusInfo}><LastNote /></div>
-        <div className={styles.errorLog}><ErrorLog /></div>
+        <div className={styles.statusInfo}></div>
+        <div className={styles.errorLog}></div>
       </div>
     </AudioProvider>
   );
